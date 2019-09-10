56c824c2c840e85c703edea314243fc7017c6022
Omer Latif <olatif@u.rochester.edu>
2019-09-10 17:02:47 -0400

    got login and register new user to work

---
+++ Blahgger/CustomPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using Blahgger.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Blahgger
+{
+
+    public class CustomPrincipal : ICustomPrincipal
+    {
+        public IIdentity Identity { get; private set; }
+
+        public bool IsCurrentUser(int id)
+        {
+            if (id == 0) return false;
+            //if currentLoggedinUserId == id return true
+
+            return false;
+        }
+
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomPrincipal(string email)
+        {
+            this.Identity = new GenericIdentity(email, "Custom");
+        }
+        public int Id { get; set; }
+    }
+}
